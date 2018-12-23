8c4d29415a056118e34665685fd7da35c5a5e544
EricJayLsh <1664996418@qq.com>
2018-12-23 18:11:48 +0800

    [ADD] add 0003和0006

---
+++ code/com/leetcode/solutions/NO_0003_无重复字符的最长子串/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Solution
+{
+
+    /*
+   * s:指定字符串
+   * 思路：存储当前子字符串，并判断当前字符是否已经存在，则移除，继续下次遍历。
+  */
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> set = new HashSet<char>();
+        int tar = 0, i = 0, j = 0;
+        int len = s.Length;
+        while (i < len && j < len)
+        {
+            if (!set.Contains(s[j]))
+            {
+                set.Add(s[j++]);
+                tar = Math.Max(tar, j - i);
+            }
+            else
+            {
+                set.Remove(s[i++]);
+            }
+        }
+        return tar;
+
+    }
+
+    // 效率最高的100ms
+    public int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> dic = new Dictionary<char, int>();
+        int maxstart = 0; int maxend = 0; int maxLength = 0;
+        int startTemp = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dic.ContainsKey(s[i]))
+                dic.Add(s[i], i);
+            else
+            {
+                if (dic[s[i]] >= startTemp)
+                {
+                    int length = i - 1 - startTemp + 1;
+                    if (length > maxLength)
+                    {
+                        maxstart = startTemp;
+                        maxend = i - 1;
+                        maxLength = length;
+                    }
+                    startTemp = dic[s[i]] + 1;
+                }
+                dic[s[i]] = i;
+            }
+        }
+        int len = s.Length - startTemp;
+        if (len > maxLength)
+            maxLength = len;
+        return maxLength;
+    }
+
+
+
+
+
+}
---
+++ code/com/leetcode/solutions/NO_0006_Z字形变换/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Solution
+{
+    /*
+  * s:指定字符串
+  * numRows:指定行数
+  * 思路：每一行的字符用数组存储，遍历字符串，通过变量判断字符应该存储到哪一个数组中，注意问题。
+ */
+    public string Convert(string s, int numRows)
+    {
+        StringBuilder res = new StringBuilder();
+        if (numRows == 1)
+        {
+
+            return s;
+        }
+        else
+        {
+
+            Dictionary<int, List<char>> allData = new Dictionary<int, List<char>>();
+            for (int i = 0; i < numRows; i++)
+            {
+                allData.Add(i, new List<char>());
+            }
+            int strIndex = 0;
+            bool gor = true;
+            for (int j = 0; j < s.Length; j++)
+            {
+                allData[strIndex].Add(s[j]);
+                if (gor)
+                {
+                    strIndex++;
+                }
+                else
+                {
+                    strIndex--;
+                }
+
+                if (strIndex == 0)
+                {
+                    gor = true;
+                }
+                else if (strIndex == numRows - 1)
+                {
+                    gor = false;
+                }
+            }
+
+            for (int k = 0; k < allData.Count; k++)
+            {
+                string item = string.Join("", allData[k]);
+                res.Append(item);
+            }
+        }
+
+        return res.ToString();
+
+    }
+}
