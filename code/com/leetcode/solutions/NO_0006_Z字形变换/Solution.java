5ec49124e032225c1b09f45a3880e50d5554ecaf
hikari <cmemory@aliyun.com>
2019-01-06 12:24:27 +0800

    [ADD]add 0006

---
+++ code/com/leetcode/solutions/NO_0006_Z字形变换/Solution.java
@@ -0,0 +1,49 @@
+package com.leetcode.solutions.NO_0006_Z字形变换;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gang.xu01@hand-china.com on 2018/12/27
+ */
+public class Solution {
+
+    /**
+     * 解法：
+     *
+     * @param s 输入字符串
+     * @param numRows 显示多少行
+     * @return
+     */
+    public String convert(String s, int numRows) {
+
+        if (numRows <= 0) throw new RuntimeException("error");
+        else if (numRows == 1) return s;
+
+        StringBuilder returnStr = new StringBuilder();
+        List<StringBuffer> sbList = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+            sbList.add(new StringBuffer());
+        }
+
+        boolean down = false;
+        int row = 0;
+        for (char c : s.toCharArray()) {
+            sbList.get(row).append(c);
+            if(row == 0 || row == numRows - 1) down = !down;
+            if(down) row++; else row--;
+        }
+
+        for (StringBuffer sb : sbList) {
+            returnStr.append(sb);
+        }
+
+        return returnStr.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
