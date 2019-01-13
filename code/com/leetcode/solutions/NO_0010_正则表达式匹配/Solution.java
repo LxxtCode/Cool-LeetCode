76a51a150380e87f16f04fbfe4034eb98c04c643
hikari <cmemory@aliyun.com>
2019-01-13 20:37:29 +0800

    [ADD]add 0010

---
+++ code/com/leetcode/solutions/NO_0010_正则表达式匹配/Solution.java
@@ -0,0 +1,18 @@
+package com.leetcode.solutions.NO_0010_正则表达式匹配;
+
+/**
+ * Created by gang.xu01@hand-china.com on 2019/1/13
+ */
+public class Solution {
+
+    /**
+     * 解法：这题太瓜了，不想说话。。。
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        if(s.matches(p)) return true;
+        else return false;
+    }
+}
