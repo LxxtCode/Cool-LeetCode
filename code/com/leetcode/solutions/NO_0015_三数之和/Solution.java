d22e4da82c1655f2e363ecc8e075d6c49e9b1b17
hikari <cmemory@aliyun.com>
2019-02-01 10:10:31 +0800

    [UPDATE]update 0015

--- code/com/leetcode/solutions/NO_0015_三数之和/Solution.java
+++ code/com/leetcode/solutions/NO_0015_三数之和/Solution.java
@@ -1,8 +1,6 @@
 package com.leetcode.solutions.NO_0015_三数之和;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by gang.xu01@hand-china.com on 2019/1/19
@@ -16,17 +14,35 @@ public class Solution {
      * @return
      */
     public List<List<Integer>> threeSum(int[] nums) {
-        int i, j, k;
-        List<List<Integer>> rl = new ArrayList<>();
-        List<Integer> l = new ArrayList<>();
-        // 对数组进行排序
+        List<List<Integer>> list = new ArrayList<>();
+        int len = nums.length;
+        if (len < 3) return list;
         Arrays.sort(nums);
-
-        return null;
+        int max = nums[len - 1];
+        if (max < 0) return list;
+        for (int i = 0; i < len - 2; ) {
+            if (nums[i] > 0) break;
+            if (nums[i] + 2 * max < 0) {
+                while (nums[i] == nums[++i] && i < len - 2) ;
+                continue;
+            }
+            int left = i + 1, right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (nums[left] == nums[++left] && left < right) ;
+                    while (nums[right] == nums[--right] && left < right) ;
+                } else if (sum < 0) ++left;
+                else --right;
+            }
+            while (nums[i] == nums[++i] && i < len - 2) ;
+        }
+        return list;
     }
 
     public static void main(String[] args) {
-        new Solution().threeSum(new int[]{-14, 2, 3, 4});
+        System.out.println(new Solution().threeSum(new int[]{-5, 3, 2}));
     }
 
 }
